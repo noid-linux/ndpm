e71b95559649cc8bf95d9cb63be5d9894e2ea5ed
Naz <ndpm13@ch-naseem.com>
2025-08-06 11:06:27 +0100

    ✨feat: add scaffolding for the xbps wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 mod args;
+mod xbps;
 
 pub use crate::args::*;
+pub use crate::xbps::*;
---
+++ src/xbps.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, Default)]
+pub struct Xbps {}
+
+impl Xbps {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
