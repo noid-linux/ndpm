27cd1dbb0124a14900f3cdba465f4abf9ef522a6
Naz <ndpm13@ch-naseem.com>
2025-08-06 11:28:04 +0100

    ✨feat: add basic xbps wrapper functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+use ndpm::{Cli, Command, Xbps};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    let xbps = Xbps::new();
+
+    match args.command {
+        Command::Install(args) => xbps.install(args)?,
+        Command::Update => xbps.update()?,
+        Command::Upgrade(args) => xbps.upgrade(args)?,
+        Command::Remove(args) => xbps.remove(args)?,
+        Command::Search(args) => xbps.search(args)?,
+    }
+
+    Ok(())
 }
--- src/xbps.rs
+++ src/xbps.rs
@@ -1,8 +1,89 @@
+use std::process::Command;
+
+use crate::{InstallArgs, RemoveArgs, SearchArgs, UpgradeArgs};
+
 #[derive(Debug, Default)]
 pub struct Xbps {}
 
 impl Xbps {
     pub fn new() -> Self {
         Self {}
+    }
+    pub fn install(&self, args: InstallArgs) -> Result<(), Box<dyn std::error::Error>> {
+        let mut options: Vec<&str> = vec![];
+
+        if args.yes {
+            options.push("-y");
+        }
+
+        Command::new("sudo")
+            .arg("xbps-install")
+            .args(options)
+            .args(args.packages)
+            .spawn()?
+            .wait()?;
+
+        Ok(())
+    }
+    pub fn update(&self) -> Result<(), Box<dyn std::error::Error>> {
+        Command::new("sudo")
+            .arg("xbps-install")
+            .arg("-S")
+            .spawn()?
+            .wait()?;
+
+        Ok(())
+    }
+    pub fn upgrade(&self, args: UpgradeArgs) -> Result<(), Box<dyn std::error::Error>> {
+        let mut options: Vec<&str> = vec![];
+
+        if args.yes {
+            options.push("-y");
+        }
+
+        // Upgrade xbps itself
+        Command::new("sudo")
+            .arg("xbps-install")
+            .arg("-u")
+            .args(&options)
+            .arg("xbps")
+            .spawn()?
+            .wait()?;
+
+        Command::new("sudo")
+            .arg("xbps-install")
+            .arg("-u")
+            .args(options)
+            .args(args.packages)
+            .spawn()?
+            .wait()?;
+
+        Ok(())
+    }
+    pub fn remove(&self, args: RemoveArgs) -> Result<(), Box<dyn std::error::Error>> {
+        let mut options: Vec<&str> = vec![];
+
+        if args.yes {
+            options.push("-y");
+        }
+
+        Command::new("sudo")
+            .arg("xbps-remove")
+            .arg("-R")
+            .args(options)
+            .args(args.packages)
+            .spawn()?
+            .wait()?;
+
+        Ok(())
+    }
+    pub fn search(&self, args: SearchArgs) -> Result<(), Box<dyn std::error::Error>> {
+        Command::new("xbps-query")
+            .arg("-Rs")
+            .arg(args.package)
+            .spawn()?
+            .wait()?;
+
+        Ok(())
     }
 }
