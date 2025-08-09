d9cb01d2da1b72ce76dbc6dd9b8b9bd445a8482f
Naz <ndpm13@ch-naseem.com>
2025-08-09 15:17:40 +0100

    🐛fix: collapse nested if block

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ pub use crate::error::*;
 pub use crate::xbps::*;
 
 pub fn is_root() -> bool {
-    if let Ok(output) = std::process::Command::new("id").arg("-u").output() {
-        if let Ok(uid_str) = String::from_utf8(output.stdout) {
-            return uid_str.trim() == "0";
-        }
+    if let Ok(output) = std::process::Command::new("id").arg("-u").output()
+        && let Ok(uid_str) = String::from_utf8(output.stdout)
+    {
+        return uid_str.trim() == "0";
     }
     false
 }
